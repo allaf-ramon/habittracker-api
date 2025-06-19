7ffa260d7cff1e038ec297e11eb79b08e85f0cfb
Allaf Ramon <allafr1999@gmail.com>
2025-06-18 23:05:39 -0300

    adiciona configuracao inicial do projeto habittracker-api - dev

---
+++ src/main/java/br/com/habittracker/api/HabittrackerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.habittracker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabittrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabittrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/habittracker/api/HabittrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.habittracker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabittrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
