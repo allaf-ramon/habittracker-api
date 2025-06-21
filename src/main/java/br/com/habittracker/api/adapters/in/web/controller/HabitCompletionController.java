23f279ad81ba8f58678c4b70890b12558e614dfe
Allaf Ramon <allafr1999@gmail.com>
2025-06-21 03:26:51 -0300

    feat: adiciona controlador para gerenciamento de completacao de habitos

---
+++ src/main/java/br/com/habittracker/api/adapters/in/web/controller/HabitCompletionController.java
@@ -0,0 +1,29 @@
+package br.com.habittracker.api.adapters.in.web.controller;
+
+import br.com.habittracker.api.domain.port.in.MarkCompletionUseCase;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/v1/habits/{habitId}/completions")
+public class HabitCompletionController {
+    private final MarkCompletionUseCase markCompletionUseCase;
+
+    public HabitCompletionController(MarkCompletionUseCase markCompletionUseCase) {
+        this.markCompletionUseCase = markCompletionUseCase;
+    }
+
+    @PostMapping("/{date}")
+    public ResponseEntity<Void> markAsCompleted(@PathVariable Long habitId, @PathVariable LocalDate date) {
+        markCompletionUseCase.markAsCompleted(habitId, date);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{date}")
+    public ResponseEntity<Void> markAsNotCompleted(@PathVariable Long habitId, @PathVariable LocalDate date) {
+        markCompletionUseCase.markAsNotCompleted(habitId, date);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/br/com/habittracker/api/application/configuration/DomainConfiguration.java
+++ src/main/java/br/com/habittracker/api/application/configuration/DomainConfiguration.java
@@ -1,6 +1,8 @@
 package br.com.habittracker.api.application.configuration;
 
+import br.com.habittracker.api.domain.port.out.CompletionRepositoryPort;
 import br.com.habittracker.api.domain.port.out.HabitRepositoryPort;
+import br.com.habittracker.api.domain.service.CompletionService;
 import br.com.habittracker.api.domain.service.HabitService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +12,10 @@ public class DomainConfiguration {
     @Bean
     public HabitService habitService(HabitRepositoryPort habitRepositoryPort) {
         return new HabitService(habitRepositoryPort);
+    }
+    @Bean
+    public CompletionService completionService(CompletionRepositoryPort completionRepositoryPort,
+                                               HabitRepositoryPort habitRepositoryPort) {
+        return new CompletionService(completionRepositoryPort, habitRepositoryPort);
     }
 }
